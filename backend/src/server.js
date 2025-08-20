7ffcbfc74b5295300cb05a62f4761a1d560df300
sriharinewuser <148181149+sriharinewuser@users.noreply.github.com>
2025-08-20 06:25:35 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,44 +6,30 @@ import cors from 'cors';
 import foodRouter from './routers/food.router.js';
 import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
-// import uploadRouter from './routers/upload.router.js';
-
-
 import { dbconnect } from './config/database.config.js';
-import path,{ dirname } from 'path'
-dbconnect();
 
+dbconnect();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const app = express();
 app.use(express.json());
+
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000','https://foodiespaw.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://foodiespaw.vercel.app'],
   })
 );
 
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-//
 
-const publicFolder = path.join(__dirname, 'public');
-app.use(express.static(publicFolder));
+// ✅ no need for static/public if frontend is deployed separately
 
-app.get('*', (req, res) => {
-  const indexFilePath = path.join(publicFolder, 'index.html');
-  res.sendFile(indexFilePath);
-});
-
-
-
-
-const PORT = process.env.Port ||5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
