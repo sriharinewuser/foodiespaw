e3418a2b0fe0eee20cadee7408f712273d7ea570
sriharinewuser <sri948653@gmail.com>
2025-08-21 06:18:40 +0100

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.get('/api/health', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
