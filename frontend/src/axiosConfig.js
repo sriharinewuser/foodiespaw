febd706e6b0bf9dea0f90333f24386625ce9e3f0
sriharinewuser <148181149+sriharinewuser@users.noreply.github.com>
2025-08-24 15:55:40 +0100

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 axios.defaults.baseURL =
 
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : 'https://foodiespaw.onrender.com';
+  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : 'process.env.REACT_APP_API_URL';
