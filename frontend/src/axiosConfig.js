d38d44c0a1ce79160d2d86093405cf35aaa4789c
sriharinewuser <sri948653@gmail.com>
2025-08-24 07:35:03 +0100

    first commit

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =
-
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : 'https://foodiespaw.onrender.com';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 
+  (process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : 'https://foodiespaw.onrender.com');
